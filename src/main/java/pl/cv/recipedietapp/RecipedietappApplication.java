6f8992c7d587237372229a44f172bdd482c32afe
Krzysiek <k.dyr89@gmail.com>
2020-07-11 10:22:32 +0200

    initialize project

---
+++ src/main/java/pl/cv/recipedietapp/RecipedietappApplication.java
@@ -0,0 +1,13 @@
+package pl.cv.recipedietapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipedietappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipedietappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/cv/recipedietapp/RecipedietappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cv.recipedietapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipedietappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
