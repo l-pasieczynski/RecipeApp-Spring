41934724fe9bd02834bb318641f71f0cc7a4fa95
lukasz-pasieczynski <l.pasieczynski@gmail.com>
2020-07-13 23:10:16 +0200

    SpringSecurity - custom user to fix (500)

--- src/main/java/pl/cv/recipedietapp/controller/UserController.java
+++ src/main/java/pl/cv/recipedietapp/controller/UserController.java
@@ -1,10 +1,12 @@
 package pl.cv.recipedietapp.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.cv.recipedietapp.entity.User;
+import pl.cv.recipedietapp.service.CurrentUser;
 import pl.cv.recipedietapp.service.UserService;
 
 @Controller
@@ -21,11 +23,18 @@ public class UserController {
     @ResponseBody
     public String createUser() {
         User user = new User();
-        user.setUsername("admin");
+        user.setUsername("Lucas");
         user.setPassword("admin");
         user.setEmail("admin@admin.pl");
         userService.saveUser(user);
         return "admin";
+    }
+
+    @GetMapping("admin")
+    @ResponseBody
+    public String admin (@AuthenticationPrincipal CurrentUser currentUser){
+        User entityUser = currentUser.getUser();
+        return "Hello " + entityUser.getUsername();
     }
 
 
---
+++ src/main/java/pl/cv/recipedietapp/service/CurrentUser.java
@@ -0,0 +1,20 @@
+package pl.cv.recipedietapp.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import java.util.Collection;
+
+public class CurrentUser extends User {
+    private final pl.cv.recipedietapp.entity.User user;
+
+    public CurrentUser(String username, String password,
+                       Collection<? extends GrantedAuthority> authorities,
+                       pl.cv.recipedietapp.entity.User user) {
+        super(username, password, authorities);
+        this.user = user;
+    }
+
+    public pl.cv.recipedietapp.entity.User getUser() {
+        return user;
+    }
+}
