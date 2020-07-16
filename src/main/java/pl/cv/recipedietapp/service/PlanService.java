7286aa722bef6b8c2059a74f1adbaefa91b5ee1a
lukasz-pasieczynski <l.pasieczynski@gmail.com>
2020-07-17 00:23:26 +0200

    services

--- src/main/java/pl/cv/recipedietapp/service/PlanService.java
+++ src/main/java/pl/cv/recipedietapp/service/PlanService.java
@@ -1,6 +1,10 @@
 package pl.cv.recipedietapp.service;
 
 import org.springframework.stereotype.Service;
+import pl.cv.recipedietapp.entity.Plan;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @Service
 public class PlanService {
@@ -9,4 +13,22 @@ public class PlanService {
     public PlanService(PlanRepository planRepository) {
         this.planRepository = planRepository;
     }
+
+    public void deletePlan(Long planId) {
+        Plan plan = findById(planId);
+        planRepository.delete(planId);
+    }
+
+    public void deleteAllUserPlans(){
+        planRepository.deleteAll();
+    }
+
+    public Plan findByPlanId(Long planId) {
+        return planRepository.findById(planId);
+    }
+
+    public List<Plan> findAllUserPlans(Long userId) {
+        return planRepository.findAll(userId);
+    }
 }
+
--- src/main/java/pl/cv/recipedietapp/service/RecipeService.java
+++ src/main/java/pl/cv/recipedietapp/service/RecipeService.java
@@ -1,6 +1,10 @@
 package pl.cv.recipedietapp.service;
 
 import org.springframework.stereotype.Service;
+import pl.cv.recipedietapp.entity.Plan;
+import pl.cv.recipedietapp.entity.Recipe;
+
+import java.util.List;
 
 @Service
 public class RecipeService {
@@ -8,5 +12,26 @@ public class RecipeService {
 
     public RecipeService(RecipeRepository recipeRepository) {
         this.recipeRepository = recipeRepository;
+    }
+
+    public void deleteRecipe(Long recipeId) {
+        Recipe recipe = findById(recipeId);
+        recipeRepository.delete(recipeId);
+    }
+
+    public void deleteAllUserRecipies(){
+        recipeRepository.deleteAll();
+    }
+
+    public Recipe findByRecipeId(Long recipeId) {
+        return recipeRepository.findById(recipeId);
+    }
+
+    public List<Recipe> findAllUserRecipies(Long userId) {
+        return recipeRepository.findAll(userId);
+    }
+
+    public List<Recipe> findAll() {
+        return recipeRepository.findAll();
     }
 }
